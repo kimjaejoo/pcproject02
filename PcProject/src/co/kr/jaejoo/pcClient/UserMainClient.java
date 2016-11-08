48cfa62de89a104c484b3c861a5f458ba7862f87
kimjaejoo <kjj0710@naver.com>
2016-11-08 19:13:13 +0900

    11. 08 commit : Tacademy에서 배운 내용을 토대로 자바프로그램 생성을 위한 workspace 생성 - 관리자 페이지를 웹으로 연결하기 위해 dynamicweb project 생성

---
+++ PcProject/src/co/kr/jaejoo/pcClient/UserMainClient.java
@@ -0,0 +1,21 @@
+package co.kr.jaejoo.pcClient;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class UserMainClient extends JFrame {
+
+	public UserMainClient() {
+		setTitle("사용자를 컨트롤하는 화면입니다.");
+		setSize(1600, 900);
+		Dimension framesize = this.getSize();
+		Dimension windowsize = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((windowsize.width - framesize.width) / 2, (windowsize.height - framesize.height) / 2);
+		
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+}
